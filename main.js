3c97567deced0c5ceeba05030e0dc8dd5dfafdeb
x20surya <77159015+x20surya@users.noreply.github.com>
2023-12-02 10:54:34 +0530

    Update main.js

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ import './style.css'
 
 import * as THREE from 'three';
 import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
 const scene = new THREE.Scene();
 const camera =new THREE.PerspectiveCamera(75,window.innerWidth/window.innerHeight,0.1,1000);
